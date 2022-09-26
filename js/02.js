553f9a2c91492fd1122d212aee77688c2d8489ee
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-09-26 00:59:51 -0300

    let y const

---
+++ js/02.js
@@ -0,0 +1,16 @@
+//variables con let
+
+let producto = 'Audifonos Gamer' //  iniciar variable y asignarele valor
+
+let disponible; //iniciamos la variable pero sin valor
+
+producto = true //reasignano el valor de la variable
+
+disponible = true
+
+//si qisiera definir varias variables se puede
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'computadras';
+
+    let nombreProducto = 'MonitorHD'
---
+++ js/03.js
@@ -0,0 +1,15 @@
+//variables con cosnt
+const producto = 'Audifonos Gamer' //  iniciar variable y asignarele valor
+
+//const disponible; // no se puede inicializar la variable sin valor
+
+// producto = true //reasignano el valor de la variable
+
+disponible = true
+
+//si qisiera definir varias variables se puede
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'computadras';
+
+    let nombreProducto = 'MonitorHD'
