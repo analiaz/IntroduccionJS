76a714174d103c4fdf81a84d6f15342dcb753f89
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-09-26 00:40:15 -0300

    variables y comentarios

--- js/01.js
+++ js/01.js
@@ -1,2 +1,15 @@
 // Variables
 
+var producto = 'Audifonos Gamer' //  iniciar variable y asignarele valor
+
+var disponible; //iniciamos la variable pero sin valor
+
+producto = true //reasignano el valor de la variable
+
+disponible = true
+
+//si qisiera definir varias variables se puede
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'computadras';
+
