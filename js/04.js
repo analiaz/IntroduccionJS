45a3aad49b79b08ebba8e929f2c905c8e9613780
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-09-26 01:18:13 -0300

    strings

---
+++ js/04.js
@@ -0,0 +1,10 @@
+//strings o cadenas de texto
+const producto = "Monitor de 20 pulgadas" 
+
+//constructor
+const producto2 = String('Monitor de 30 puldadas');
+const producto3 = new String('Monitor de 50 puldadas');
+
+console.log(producto)
+console.log(producto2)
+console.log(typeof producto3) // es un objeto (typeof te dice de que tipo es)
