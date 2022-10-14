01d37fdd1ed1f51b89a1c63d70c54677a4034ca2
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-10-13 22:34:48 -0300

    Objeto Math

---
+++ js/06.js
@@ -0,0 +1,14 @@
+//Objetos math
+
+let resul;
+
+resul = Math.PI
+resul = Math.round(2.5) //redondea
+resul = Math.ceil(2.1) //ceil siempre redondea hacia arriba
+resul = Math.floor(2.1) //floor siempre redondea hacia abajo
+resul = Math.sqrt(144) //raiz cuadrada
+resul = Math.abs(-200)
+resul = Math.min(1, 23, 44, 0)
+resul = Math.max(1, 23, 44, 0)
+resul = Math.random // por lo general nunca llega a mayor que uno
+resul = Math.floor( Math.random()*30)
