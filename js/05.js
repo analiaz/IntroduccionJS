1ba2c9019a00a4f5e00cb9ebdb210c3f3824b0e3
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-10-13 22:24:36 -0300

    operacionesNumeros

---
+++ js/05.js
@@ -0,0 +1,10 @@
+//numeros
+
+const num1 = 100
+const num2 = 200
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2); //modulo residuo 
