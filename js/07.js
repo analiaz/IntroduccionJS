d160638ece79bc14e03a2635bc9bb58e885b1849
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-10-13 22:55:51 -0300

    ordenoperaciones

---
+++ js/07.js
@@ -0,0 +1,16 @@
+//Orden de las Operaciones
+
+let resul;
+
+resul = (20 * 30) * 2
+
+//Incremento
+
+let puntaje
+
+//Incremento en 1 
+
+puntaje++ //10 y luego incrementa
+++puntaje // 11 incrementa y luego pega
+
+puntaje +=10 //incrementa en 10
