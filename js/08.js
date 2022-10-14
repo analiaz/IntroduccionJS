eb280b40df5b369e974cdf393e39c4c31130bf55
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-10-13 23:03:19 -0300

    Strings Literales

---
+++ js/08.js
@@ -0,0 +1,9 @@
+const nom = "ana"
+const email = "correo@gmail.com"
+
+// concatenacion
+console.log("nombre Cliente: " + nombre + " " + email)
+
+//template strings - Strings literals (es lo nuevo para no concatenar)
+//           string        : variable
+console.log(`Nombre Cliente: ${nombre} Email ${email}`)
