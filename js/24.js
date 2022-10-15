916414bccd94769df50064f0b4de1febc92c8574
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-10-14 23:41:21 -0300

    While| Do while

--- js/24.js
+++ js/24.js
@@ -10,5 +10,18 @@ for(let i = 0; i <Carrito.length; i++) {
 
 //while loop
 
+let i = 0
+while( i < 10){
+ console.log(i)
+ i++
+}
+
 
 //Do while Loop
+
+let i = 0
+
+do {
+
+    i++
+} while(i < 10)
