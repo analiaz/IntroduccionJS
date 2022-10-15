9d02ad7933163ddebdca37d74db66f841fb57337
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-10-14 22:51:48 -0300

    func

---
+++ js/19.js
@@ -0,0 +1,15 @@
+function sumar(n1, n2) {
+    return n1 + n2
+}
+
+const res = sumar(2,3);
+console.log(res)
+
+let total = 0
+
+function agregarAlCarrito(precio){
+    return total += precio
+}
+
+total = agregarAlCarrito(899)
+
