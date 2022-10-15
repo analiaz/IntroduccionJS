d0c998e586b980d7301464edde498ddaa09e1542
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-10-14 23:22:30 -0300

    IF

---
+++ js/22.js
@@ -0,0 +1,13 @@
+//estructuras de control
+
+// que sean === sign es mas estricto revisa el valor y el tipo de datos
+
+const puntaje = 1000
+
+if(puntaje === 1000){
+    console.log("si")
+} else if (){
+    console
+} else {
+    
+}
