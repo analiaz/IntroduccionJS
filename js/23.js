24aa210a61e4ce6e820976471bdc9d23b23be788
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-10-14 23:25:03 -0300

    Switch CASE

---
+++ js/23.js
@@ -0,0 +1,10 @@
+//Switch CASE
+
+const metodoDePago = 'tarjeta'
+
+switch(metodoDePago){
+    case 'tarjeta': console.log('pagaste con tarjeta')
+        break;
+    default: console.log('aun no has pagado')
+        break
+}
