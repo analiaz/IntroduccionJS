7f6bc15740bb024d143da8f7b88119d7b81f0dce
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-10-14 23:37:35 -0300

    FOR

---
+++ js/24.js
@@ -0,0 +1,14 @@
+//for loop
+
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+for(let i = 0; i <Carrito.length; i++) {
+    console.log(carrito[i].nombre)
+}
+
+//while loop
+
+
+//Do while Loop
