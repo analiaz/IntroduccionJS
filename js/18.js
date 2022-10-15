a3fea537d81e86bd8dbfeed93002ea2c43ddb32f
Analia Lujan Zunino <analia.zunino@accenture.com>
2022-10-14 22:31:06 -0300

    parametros y argumentos

---
+++ js/18.js
@@ -0,0 +1,13 @@
+
+
+//sumar parametros
+function sumar(num1, num2) { //num1 y 2 sonparametros
+    console.log(num1 + num2);
+}
+
+sumar(10, 10); //argumentos o valores reales
+
+
+const sumar2 = function() {
+    console.log(3+3)
+}
